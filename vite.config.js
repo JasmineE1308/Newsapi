36aa515bb02bf3319d9fd78c693508d9da845232
Jasmine <jasminechristo2@gmail.com>
2025-02-19 20:09:23 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server:{
     proxy: {
-      '/api': 'http://localhost:5174/newsapi/'
+      '/api': 'http://localhost:5174/'
     }
   },
   base: "/newsapi/",
